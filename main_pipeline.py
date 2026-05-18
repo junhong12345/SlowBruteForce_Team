c80a63c81fc591a497fc49f13a643646cbdcd225
junhong12345 <minjunhong95@gmail.com>
2026-05-18 20:20:03 +0900

    Update main_pipeline.py

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,2 +1,4 @@
-import os 
-os.system("python3 Logic1.py && python3 Logic2.py")
+import os
+os.system("python3 Logic1.py && python3 Logic2.py") #표준편차 
+os.system("python3 Jitter.py && python3 Jitter_CV_Detector.py") #CV, Jitter
+
